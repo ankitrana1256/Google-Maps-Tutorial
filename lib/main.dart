ac3cf4c827a6a1f18ff56cd9ac14e97097a071d2
Ankit <ankitrana5000nd@gmail.com>
2023-02-19 15:49:57 +0530

    StreamingPosition

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,15 @@
+import 'dart:async';
+
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:google_maps_flutter/google_maps_flutter.dart';
+import 'package:location/location.dart';
 
 void main() {
+  // To make statusbar visible
+  SystemChrome.setSystemUIOverlayStyle(
+    const SystemUiOverlayStyle(statusBarColor: Colors.transparent),
+  );
   runApp(const MyApp());
 }
 
@@ -15,7 +23,7 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
-      home: MyGoogleMap(),
+      home: const MyGoogleMap(),
       debugShowCheckedModeBanner: false,
     );
   }
@@ -29,38 +37,98 @@ class MyGoogleMap extends StatelessWidget {
     return SafeArea(
       child: Scaffold(
         appBar: AppBar(
-          elevation: 0,
+          elevation: 2,
           title: const Text("Google Map"),
           centerTitle: true,
           backgroundColor: Colors.green[300],
         ),
-        body: GoogleMapWidget(),
+        body: const GoogleMapWidget(),
       ),
     );
   }
 }
 
-// Add Google Maps api in android.manifest file (android\app\src\main\AndroidManifest.xml)
-// flutter pub add google_maps_flutter
-// Change sdk version to support GoogleMap
-// In my case minsdkversion is 20
-class GoogleMapWidget extends StatelessWidget {
+// Converting this widget to stateful so it can update marker position on Google Map
+// For listening to locations we need a location package
+// flutter pub add location
+class GoogleMapWidget extends StatefulWidget {
   const GoogleMapWidget({super.key});
 
   @override
-  Widget build(BuildContext context) {
-    var initialCameraPosition = const CameraPosition(
-      target: LatLng(28.408913, 77.317787),
-      tilt: 40,
-      zoom: 16,
-    );
-    return GoogleMap(
-      initialCameraPosition: initialCameraPosition,
+  State<GoogleMapWidget> createState() => _GoogleMapWidgetState();
+}
 
-      // This is to remove the compass which comes in default
-      compassEnabled: false,
-      // This is to remove the zoom buttons
-      zoomControlsEnabled: false,
+class _GoogleMapWidgetState extends State<GoogleMapWidget> {
+  var initialCameraPosition = const CameraPosition(
+    target: LatLng(28.408913, 77.317787),
+    zoom: 0,
+  );
+
+  // An instance of currentLocation
+  Location currentLocation = Location();
+
+  // Should be initialised later when streaming starts
+  late StreamSubscription streamLocation;
+  final Set<Marker> markers = {};
+  GoogleMapController? googleMapController;
+
+  // We need to listen for our coordinates before build
+  @override
+  void initState() {
+    super.initState();
+    streamMyLocation();
+  }
+
+  // Since we are streaming our coordinates continuously we also have to delete old coordinates
+  @override
+  void dispose() {
+    googleMapController?.dispose();
+    streamLocation.cancel();
+    super.dispose();
+  }
+
+  // Function to keep track of my location
+  void streamMyLocation() {
+    streamLocation =
+        currentLocation.onLocationChanged.listen((LocationData loc) {
+      CameraUpdate.newCameraPosition(
+        CameraPosition(
+          target: LatLng(loc.latitude ?? 0.0, loc.longitude ?? 0.0),
+        ),
+      );
+      setState(() {
+        initialCameraPosition = CameraPosition(
+          target: LatLng(loc.latitude ?? 0.0, loc.longitude ?? 0.0),
+          tilt: 40,
+          zoom: 16,
+        );
+        var myloc = Marker(
+            markerId: const MarkerId("Your Position"),
+            position: LatLng(loc.latitude ?? 0.0, loc.longitude ?? 0.0));
+        markers.add(myloc);
+      });
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      floatingActionButton: FloatingActionButton(onPressed: () {
+        // Trigger to animate location
+        googleMapController!.animateCamera(
+          CameraUpdate.newCameraPosition(initialCameraPosition),
+        );
+      }),
+      body: GoogleMap(
+        initialCameraPosition: initialCameraPosition,
+        compassEnabled: false,
+        zoomControlsEnabled: false,
+        onMapCreated: (controller) {
+          googleMapController = controller;
+        },
+        // We defined a set of marker so we can have multiple markers
+        markers: markers,
+      ),
     );
   }
 }
